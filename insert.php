db94afd49510d7d743ad395fc8e51b2456f2d665
manojgardi15 <manojgardi157@gmail.com>
2022-09-25 14:03:15 +0530

    add validation

--- insert.php
+++ insert.php
@@ -26,4 +26,54 @@ $sql = "INSERT INTO newemployee (first_name,last_name,gender,birth_date,aadharca
      }
      mysqli_close($conn);
 }
+
+
+// define variables and set to empty values
+$first_nameErr = $last_nameErr = $emailErr = $genderErr = $websiteErr = "";
+$first_name = $last_name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["first_name"])) {
+    $first_nameErr = "Field is required";
+  } else {
+    $first_name = test_input($_POST["first_name"]);
+  }
+
+  if (empty($_POST["last_name"])) {
+    $last_nameErr = "Field is required";
+  } else {
+    $last_name = test_input($_POST["last_name"]);
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+  }
+    
+  if (empty($_POST["website"])) {
+    $website = "";
+  } else {
+    $website = test_input($_POST["website"]);
+  }
+
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
+
+  if (empty($_POST["gender"])) {
+    $genderErr = "Gender is required";
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
 ?>
